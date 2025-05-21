abff12da27405feda81b5be4d060284c3cded458
Adam Atanas <adam.atanas@ses.ai>
2025-05-21 12:10:26 -0400

    update references formatting

--- src/Chatbox.js
+++ src/Chatbox.js
@@ -587,18 +587,44 @@ const handleFindSimilarMolecules = async (details) => {
                 key={index} 
                 className={msg.type} 
                 style={{ whiteSpace: 'pre-wrap' }}>
-                <div>{msg.text}</div>
-                {msg.sources && (
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(msg.text, {
+                      ALLOWED_TAGS: [
+                        'a',          // for links
+                        'sup', 'sub',        // for superscript numbers
+                        'strong', 'em', 'br', 'p', 'ul', 'li', 'ol', 'i'
+                      ],
+                      ALLOWED_ATTR: ['href', 'target', 'rel']
+                    })
+                  }}
+                />
+                { // new backend code cites references itself
+                  /* {msg.sources && (
                   <div
                     dangerouslySetInnerHTML={{
                       __html: DOMPurify.sanitize(msg.sources, {
-                        ALLOWED_TAGS: ['a', 'strong', 'em', 'br', 'p', 'ul', 'li', 'ol'],
+                        ALLOWED_TAGS: [
+                          'a',          // for links
+                          'sup', 'sub',        // for superscript numbers
+                          'strong', 'em', 'br', 'p', 'ul', 'li', 'ol', 'i'
+                        ],
                         ALLOWED_ATTR: ['href', 'target', 'rel']
                       })
                     }}
                   />
+                )} */}
+                <>
+                  
+                </>
+                {msg.molText && (
+                  <div>
+                    <br></br>
+                    <strong>Detected chemical keywords in LLM response:</strong>
+                    <br></br>
+                    {msg.molText}
+                  </div>
                 )}
-                {msg.molText && <div>{msg.molText}</div>}
                 {msg.type === "llm-message" && msg.molecules && msg.molecules.length > 0 && (
                   <div style={{ marginTop: '10px', display: 'flex', alignItems: 'center' }}>
                     <button 
