c6dc288f4b160e5f94d1d281966060423deac6b0
Victor Novichov <victor.novichkov@ses.ai>
2025-06-12 13:04:57 -0400

    fix: added webp images back with picture objects

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -50,14 +50,20 @@ const MapPage = ({ handlePointClick }) => {
                     </p>
 
                     <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '25px', marginTop: '10px' }}>
-                        <img loading="lazy" src="/MU_About_Cluster_Numbered.jpg"
-                            alt="Molecular Universe Clusters Map"
-                            style={{
-                                maxWidth: '100%',
-                                height: 'auto',
-                                borderRadius: '8px',
-                                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
-                            }} />
+                        <picture>
+                            <source srcSet="/MU_About_Cluster_Numbered.webp" type="image/webp" />
+                            <img
+                                loading="lazy"
+                                src="/MU_About_Cluster_Numbered.jpg"
+                                alt="Molecular Universe Clusters Map"
+                                style={{
+                                    maxWidth: '100%',
+                                    height: 'auto',
+                                    borderRadius: '8px',
+                                    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
+                                }}
+                            />
+                        </picture>
                     </div>
 
                     <h3 style={{ fontWeight: 'bold', marginBottom: '15px', marginTop: '25px' }}>Cluster Descriptions</h3>
