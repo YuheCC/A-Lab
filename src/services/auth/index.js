e488b74d1ff98bae8ba2604605d4b00a3ffc3d78
FrankWangSES <feiran.wang@ses.ai>
2025-07-11 23:07:54 +0800

    feat: add backend api

--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -36,18 +36,6 @@ export async function resetPassword(data) {
     });
 }
 
-export async function forgotPassword(data) {
-    const formData = new FormData();
-    Object.keys(data).forEach(key => {
-        formData.append(key, data[key]);
-    });
-
-    return request('/forgot-password', {
-        method: "POST",
-        data: formData,
-    });
-}
-
 export async function redeemCode(data) {
     const formData = new FormData();
     Object.keys(data).forEach(key => {
@@ -68,6 +56,56 @@ export async function verify() {
 
 export async function verifyCode(params) {
     return request('/verify-register-code', {
+        method: "GET",
+        params,
+    });
+}
+
+export async function verifyRedeemCode(data) {
+    const formData = new FormData();
+    Object.keys(data).forEach(key => {
+        formData.append(key, data[key]);
+    });
+
+    return request('/redeem-new', {
+        method: "POST",
+        data: formData,
+    });
+}
+
+export async function sendEducationCode(data) {
+    const formData = new FormData();
+    Object.keys(data).forEach(key => {
+        formData.append(key, data[key]);
+    });
+
+    return request('/bind-edu-email', {
+        method: "POST",
+        data: formData,
+    });
+}
+
+export async function verifyEducationCode(params) {
+    return request('/verify-edu-code', {
+        method: "GET",
+        params,
+    });
+}
+
+export async function sendForgotPasswordCode(data) {
+    const formData = new FormData();
+    Object.keys(data).forEach(key => {
+        formData.append(key, data[key]);
+    });
+
+    return request('/forgot-password', {
+        method: "POST",
+        data: formData,
+    });
+}
+
+export async function verifyForgotPasswordCode(params) {
+    return request('/verify-forgot-password-code', {
         method: "GET",
         params,
     });
