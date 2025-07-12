082c0318d833d8c61d413db5a617bfec965779b2
FrankWangSES <feiran.wang@ses.ai>
2025-07-12 13:39:01 +0800

    feat: test

--- config/config.ts
+++ config/config.ts
@@ -3,7 +3,7 @@ import routes from "../src/routes";
 
 export default defineConfig({
   routes,
-  npmClient: 'pnpm',
+  npmClient: 'npm',
   favicons: ['https://cdn.prod.website-files.com/671b8da8cb402b2908cb39f0/671bb55d6ea8b4dc808e5cfa_fav-256.png'],
   define: {
     'BASE_URL': "https://api-sh.ses.ai",
