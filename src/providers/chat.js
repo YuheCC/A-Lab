b7b3a7c0d9a2180b22ca0a711c1da1fd83fdffcf
codywirthses <cody.wirth@ses.ai>
2025-07-15 18:26:29 -0400

    updated chat history to properly save dropdown menus
    updated chat history to properly save dropdown menus

--- src/providers/chat.js
+++ src/providers/chat.js
@@ -103,6 +103,7 @@ export const useChatStore = create(persist((set, get) => ({
                                 content: item.content || '',
                                 molText: item.molecules.join(", ") || [],
                                 molecules: item.molecules || [],
+                                extraData: item.extra_data || {},
                             })) || [],
                             activeMolecule: chat.meta_active_molecule || null,
                             foundMolecules: chat.meta_molecules || [],
