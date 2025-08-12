92881b794ee54082851961220727692d164b40fd
codywirthses <cody.wirth@ses.ai>
2025-08-12 13:26:56 -0400

    added config files
    added config files

---
+++ config/config.staging.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "umi";
+
+export default defineConfig({
+  define: {
+    'BASE_URL': "https://demo-api.ses.ai",
+    'explorer_url': "https://buy.stripe.com/test_9B66oGgL2dPh0C12Mzebu01",
+    'team_url': "https://buy.stripe.com/test_9B600iamEeTl1G572Pebu02",
+  },
+});
---
+++ config/config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "umi";
+import routes from "../src/routes";
+
+export default defineConfig({
+  routes,
+  npmClient: 'npm',
+  favicons: ['https://cdn.prod.website-files.com/671b8da8cb402b2908cb39f0/671bb55d6ea8b4dc808e5cfa_fav-256.png'],
+  outputPath: 'build',
+  define: {
+    'BASE_URL': "https://demo-api.ses.ai",
+    'explorer_url': "https://buy.stripe.com/6oE165fCb3Tf0qA5kl",
+    'team_url': "https://buy.stripe.com/dR67utfCb3TffludQS",
+  },
+  // 禁用 esbuild 来规避冲突问题
+  esbuildMinifyIIFE: false,
+  // 禁用 MFSU 来避免 esbuild 相关问题
+  mfsu: false,
+  // 禁用 esbuild 压缩，使用 terser 替代
+  jsMinifier: 'terser',
+  // 禁用 esbuild 转译，使用 babel 替代
+  jsMinifierOptions: {},
+});
