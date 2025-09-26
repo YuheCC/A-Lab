f7271cc76232ca1fa91e0b8f5553baef945c3630
FrankWangSes <feiran.wang@ses.ai>
2025-09-26 10:24:23 +0800

    feat: change table style

--- src/pages/FormulationNew/components/PropertiesTable/index.tsx
+++ src/pages/FormulationNew/components/PropertiesTable/index.tsx
@@ -14,15 +14,15 @@ interface PropertiesTableProps {
 }
 
 const defaultProperties: PropertyItem[] = [
-  { id: 1, property: 'Radial distribution function (RDF)', type: 'Structural', estimatedTime: '3 Days', rowSpan: 8 },
+  { id: 1, property: 'Radial distribution function (RDF)', type: 'Structural', estimatedTime: '3 Days', rowSpan: 7 },
   { id: 2, property: 'Coordination number', type: 'Structural' },
   { id: 3, property: 'Solvation cluster type and fraction analysis', type: 'Structural' },
   { id: 4, property: 'Diffusivity', type: 'Dynamic' },
   { id: 5, property: 'Conductivity', type: 'Dynamic' },
-  { id: 6, property: 'Ion–ion correlation', type: 'Dynamic' },
-  { id: 7, property: 'Viscosity', type: 'Dynamic' },
-  { id: 8, property: 'Density', type: 'Structural' },
-  { id: 9, property: 'Structure factor (S(q))', type: 'Structural', estimatedTime: '1 Week', rowSpan: 3 },
+  { id: 6, property: 'Viscosity', type: 'Dynamic' },
+  { id: 7, property: 'Density', type: 'Structural' },
+  { id: 8, property: 'Structure factor (S(q))', type: 'Structural', estimatedTime: '1 Week', rowSpan: 4 },
+  { id: 9, property: 'Ion–ion correlation', type: 'Dynamic' },
   { id: 10, property: 'Dynamic structure factor (S(q,ω))', type: 'Structural + Dynamic' },
   { id: 11, property: 'Residence time', type: 'Dynamic' },
   { id: 12, property: 'EDL (Electric Double Layer)', type: 'Thermodynamic', estimatedTime: '1-2 Weeks', rowSpan: 2 },
