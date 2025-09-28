a363d1c0f13b653864f64ff66f229ef1fe819bd5
FrankWangSes <feiran.wang@ses.ai>
2025-09-28 15:14:49 +0800

    feat: add enterprise locales

--- src/locales/ko/role.js
+++ src/locales/ko/role.js
@@ -4,6 +4,9 @@ export default {
     explorer: '탐험가',
     team: '팀',
     enterprise: '기업',
+    enterprise1: '기업 I',
+    enterprise2: '기업 II',
+    enterprise3: '기업 III',
     joint: '공동',
     admin: '관리자'
 }; 
