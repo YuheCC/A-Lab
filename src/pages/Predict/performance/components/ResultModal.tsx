6d9bee648f9cc2ff1a3b2f8708a100f9f9c3ee04
FrankWangSes <feiran.wang@ses.ai>
2025-10-15 14:09:02 +0800

    fix: change temperature_25_CE_label to model_result

--- src/pages/Predict/performance/components/ResultModal.tsx
+++ src/pages/Predict/performance/components/ResultModal.tsx
@@ -142,10 +142,23 @@ const ResultModal: React.FC<ResultModalProps> = ({ result, onClose }) => {
   const getProcessedResults = () => {
     // If the result object has raw API data, use it for processing
     if ((result as any).rawApiData) {
-      const apiData = (result as any).rawApiData;
+      let apiData = (result as any).rawApiData;
       let quantification_result: any = {};
       try{
         const model_result = JSON.parse(apiData?.model_result);
+        apiData = {
+          ...apiData,
+          temperature_25_CE_label: model_result?.ce_cl_result?.temperature_25_CE_label.toString(),
+          temperature_25_CE_prob: model_result?.ce_cl_result?.temperature_25_CE_prob.toString(),
+          temperature_25_CL_label: model_result?.ce_cl_result?.temperature_25_CL_label.toString(),
+          temperature_25_CL_prob: model_result?.ce_cl_result?.temperature_25_CL_prob.toString(),
+          temperature_25_CR_label: model_result?.cr_result?.temperature_25_CR_label.toString(),
+          temperature_25_CR_prob: model_result?.cr_result?.temperature_25_CR_prob.toString(),
+          temperature_45_CE_label: model_result?.ce_cl_result?.temperature_45_CE_label.toString(),
+          temperature_45_CE_prob: model_result?.ce_cl_result?.temperature_45_CE_prob.toString(),
+          temperature_45_CL_label: model_result?.ce_cl_result?.temperature_45_CL_label.toString(),
+          temperature_45_CL_prob: model_result?.ce_cl_result?.temperature_45_CL_prob.toString()
+        };
         quantification_result = model_result?.quantification_result ?? {};
       } catch (error) {
         console.error('Error parsing API data:', error);
